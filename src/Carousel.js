cdffc12fab70d8c744ce48aaef7d4e5eb95eb25c
Anna <filatovaa@yahoo.com>
2020-02-27 15:55:49 -0800

    slider layed out

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,40 +4,30 @@ import Carousel from 'react-bootstrap/Carousel'
 class ProjectCarousel extends Component {
     render() {
         return (
-            <Carousel>
+          <Carousel style={{backgroundColor:'black'}}>
   <Carousel.Item>
     <img
-      className="d-block w-100"
-      src="https://i.imgur.com/daKEcUA.png"
+      // className="d-block w-100"
+      src="https://i.imgur.com/naHFEWX.png"
       alt="First slide"
+      style={{width:400, padding: 30, display: 'grid', margin: ' 5px auto', filter:'grayscale(50%)'}}
     />
-    <Carousel.Caption>
+    <Carousel.Caption style={{position:'static'}}>
       <h3>First slide label</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
-  <Carousel.Item>
+  <Carousel.Item >
     <img
-      className="d-block w-100"
-      src="https://i.imgur.com/daKEcUA.png"
+      // className="d-block w-100"
+      src="https://i.imgur.com/cQkcfLW.png"
       alt="Third slide"
+      style={{width:400, padding: 50, display: 'grid', margin: ' 5px auto', filter:'grayscale(50%)'}}
     />
 
-    <Carousel.Caption>
+    <Carousel.Caption style={{position:'static'}}>
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://i.imgur.com/daKEcUA.png"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
@@ -46,3 +36,26 @@ class ProjectCarousel extends Component {
 }
 
 export default ProjectCarousel;
+
+
+
+{/* <Carousel style={{backgroundColor: 'black'}}>
+           
+<Carousel.Item style={{display: 'grid', gridTemplateRows: '1fr .25fr'}}> 
+    <img
+    // className="d-block w-100"
+    src="https://i.imgur.com/OksyLho.png?2"
+    alt="First slide"
+    style={{width:800, padding: 50, display: 'grid', margin: ' 5px auto', filter:'grayscale(50%)'}}
+    />
+    <Carousel.Caption style={{display: 'grid', margin: ' 5px auto'}}>
+<h3>Third slide label</h3>
+<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+</Carousel.Caption>
+
+</Carousel.Item>
+
+
+
+
+</Carousel> */}
