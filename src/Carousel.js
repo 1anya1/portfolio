99616a2048a927398937c2aa8caa739f2ffbba6c
Anna <filatovaa@yahoo.com>
2020-02-28 12:38:47 -0800

    site is layed out

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,38 +2,41 @@ import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel'
 import { Button } from "reactstrap";
 
+
  
 class ProjectCarousel extends Component {
   render() {
       return (
-          <Carousel className='carousel slide'  style={{backgroundColor:'black',interval:10000, }}>
+          <Carousel  style={{backgroundColor:'black',interval:10000, }}>
               <Carousel.Item>
                   <img className='carousel'
-                      src="https://i.imgur.com/naHFEWX.png"
+                      src="https://i.imgur.com/cQkcfLW.png"
                           alt="First slide"
                             style={{ width: '100%', maxWidth: '600px', padding: 50, display:'grid', margin: ' 5px auto', filter:'grayscale(50%)'}}
  
                     />
-                      <Carousel.Caption style={{position:'static', paddingBottom: 50}}>
-                        <h3 style={{paddingTop: 50}}>Name of Project</h3>
-                          <p>Technologies used</p>
-                          <p> a quick bio</p>
-                          <Button  className='button' size="lg" style={{width:'60%', backgroundColor: 'white', color:'black', fontWeight: 'bold', display:'grid',margin:'0 auto 100px'}}>Visit Site</Button>
+                      <Carousel.Caption style={{position:'static'}}>
+                        <h3 style={{paddingTop: 50}}>FOODIE</h3>
+                          <p>ReactJS, MongoDB, Express, HTML, CSS, Javascript, Heroku</p>
+                          <p> The Foodie app allows users to create shopping lists tied to a user account. Lists can be shared between users.</p>
+                         
+                          <Button className='button' size="lg" style={{width:'60%', backgroundColor: 'white', color:'black', fontWeight: 'bold', display:'grid',margin:'0 auto 50px'}}> <a  className='projectButton' href='https://foodie-list-app.herokuapp.com/'>Visit Site </a></Button>
+                         
                       </Carousel.Caption>
               </Carousel.Item>
 
               <Carousel.Item>
                 <img className='carousel'
-                  src="https://i.imgur.com/cQkcfLW.png"
+                   src="https://i.imgur.com/naHFEWX.png"
                     alt="First slide"
                       style={{ width: '100%', maxWidth: '600px', padding: 50, display:'grid', margin: ' 5px auto', filter:'grayscale(50%)'}}
  
               />
-                  <Carousel.Caption style={{position:'static', paddingBottom: 50}}>
-                    <h3 style={{paddingTop: 50}}>Name of Project</h3>
-                      <p>Technologies used</p>
-                      <p> a quick bio</p>
-                      <Button  className='button' size="lg" style={{width:'60%', backgroundColor: 'white', color:'black', fontWeight: 'bold', display:'grid',margin:'0 auto 100px'}}>Visit Site</Button>
+                  <Carousel.Caption style={{position:'static'}}>
+                    <h3 style={{paddingTop: 50}}>PLANTLY</h3>
+                      <p> React, Ruby on Rails, ReactStrap, PostgreSQL, AJAX, JavaScript</p>
+                      <p> Plantly is an app all about houseplants. Search for common names, learn how to recognize problems and track progress of your plants.</p>
+              <Button  className='button' size="lg" style={{width:'60%', backgroundColor: 'white', color:'black', fontWeight: 'bold', display:'grid',margin:'0 auto 50px'}}><a className='projectButton' href='https://foodie-list-app.herokuapp.com/'>Visit Site </a></Button>
                     </Carousel.Caption>
                 </Carousel.Item>
              </Carousel>
--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,11 @@ class Footer extends React.Component {
     render() {
         return (
             <div style={{backgroundColor: 'black', color: 'white'}}>
-            <h1> Contact ME </h1>
-            <h3>LinkedIn</h3>
-            <h3>Git Hub</h3>
+                <p className='footer'>Designed & Built by Anna Filatova</p>
+                <div className='links'>
+                    <div><img src='https://i.imgur.com/KTFt7Tu.png' style={{width:30}}></img></div>
+                    <div><img src='https://i.imgur.com/uXNXbZs.png' style={{width:30}}></img></div>
+                </div>
             </div>
         )
     }
