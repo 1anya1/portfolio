a4990f723ac1dddd81634828d757eae090eba7f5
Anna <filatovaa@yahoo.com>
2020-06-06 02:41:02 -0700

    changes made to resume page

--- src/Resume.js
+++ src/Resume.js
@@ -7,7 +7,7 @@ class Resume extends React.Component{
       <div className='homePage'>
         <h1 className='aboutMe'>My Resume</h1>
         <div style={{padding: '30px 0'}}>
-        <h4>Click <a href = 'https://drive.google.com/file/d/1nU4T1BH-Ai9Tm1LXVncrM9gmBZ0z8kVQ/view?usp=sharing' target="_blank">here</a> to view resume in PDF format</h4>
+        <h4>Click <a id='link' href = 'https://drive.google.com/file/d/1nU4T1BH-Ai9Tm1LXVncrM9gmBZ0z8kVQ/view?usp=sharing' target="_blank">here</a> to view resume in PDF format</h4>
         </div>
       < img className ='resumeImg' alt= '' src='https://i.imgur.com/5cQIrWG.png'></img>
       </div>
