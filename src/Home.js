99e31317315e42aeb1cfef03f7cc82b7332c6204
Anna <filatovaa@yahoo.com>
2020-06-06 02:16:01 -0700

    updates to my header

--- src/Home.js
+++ src/Home.js
@@ -11,10 +11,9 @@ class Home extends React.Component {
                     <h3 className='h3'>I'm a software engineer based in San Francisco, California. I have a passion for web design and love to create for mobile devices. </h3>
                     <h1 className='aboutMe'>About Me </h1>
                     <div className="pictureBio">
-                    <p className='bioCard'>I'm a software engineer specializing in building and designing high-quality websites and applications.
-                        I enjoy all forms of design and use multiple creative outlets to express both my ideas and myself.
-                         I am constantly gaining knowledge and inspiration in the design world both graphic and web-related. 
-                         I enjoy learning and growing in my field and expressing myself through my work. </p>
+                    <p className='bioCard'>I am a passionate creator of things on the web. I love to design my ideas thoughtfully, 
+                    then craft them into creative, yet user-friendly experiences. My background experience includes the latest development 
+                    approaches, including MVC framework, event-driven applications using AJAX, JSON, and Object-Oriented JavaScript. </p>
                          <img  alt= '' src='https://i.imgur.com/EDBgFhC.png' className='portrait'></img>
                     </div>
                     <h1 className='aboutMe'>Technologies I've worked with recently:</h1>
@@ -23,16 +22,16 @@ class Home extends React.Component {
                     <ul className="pictureBio li">
                         <div>
                             <li>JavaScript (ES6+)</li>
-                            <li>React</li>
+                            <li>React.js</li>
                             <li>Node.js</li>
                             <li>PostgreSQL</li>
                             <li>Express</li>
                         </div>
                         <div>
-                            <li>Ruby</li>
+                            <li>Ruby on Rails</li>
                             <li>Bootstrap</li>
                             <li>MongoDB</li>
-                            <li>Sass</li>
+                            <li>HTML5</li>
                             <li>AJAX</li>
                         </div>
                     </ul>
