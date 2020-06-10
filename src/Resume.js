f58d1fd488deb1cf7b29eb265007c66f73d85696
Anna <filatovaa@yahoo.com>
2020-06-09 21:49:41 -0700

    updates to PDF download

--- src/Resume.js
+++ src/Resume.js
@@ -2,17 +2,24 @@ import React from "react";
 
 
 class Resume extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
   render(){
     return(
       <div className='homePage'>
         <h1 className='aboutMe'>My Resume</h1>
         <div style={{padding: '30px 0'}}>
-        <h4>Click <a id='link' href = 'https://drive.google.com/file/d/1nU4T1BH-Ai9Tm1LXVncrM9gmBZ0z8kVQ/view?usp=sharing' target="_blank">here</a> to view resume in PDF format</h4>
+        <h4>Click <a id='link' href ='/Resume_Anna_Filatova.pdf' target="_blank">here</a> to view resume in PDF format</h4>
+       
         </div>
-      < img className ='resumeImg' alt= '' src='https://i.imgur.com/5cQIrWG.png'></img>
+      < img className ='resumeImg' alt= '' src='https://i.imgur.com/gOwMrEb.png'></img>
       </div>
     )
   }
 }
+
+  
 
 export default Resume
